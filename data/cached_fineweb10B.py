c671d8f31af7acbbec66a751dbbc45f96d673b43
KellerJordan <kellerjordan1998@gmail.com>
2024-09-28 04:05:48 +0000

    .

---
+++ data/cached_fineweb10B.py
@@ -0,0 +1,10 @@
+import os
+from huggingface_hub import hf_hub_download
+def get(fname):
+    local_dir = os.path.join(os.path.dirname(__file__), 'fineweb10B')
+    if not os.path.exists(os.path.join(local_dir, fname)):
+        hf_hub_download(repo_id="kjj0/fineweb10B-gpt2", filename=fname,
+                        repo_type="dataset", local_dir=local_dir)
+for i in range(1, 105):
+    get("fineweb_train_%06d.bin" % i)
+get("fineweb_val_%06d.bin" % 0)
