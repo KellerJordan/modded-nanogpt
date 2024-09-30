dd76ad3bb5ca0fde9a846f994e23cd716d637ced
KellerJordan <kellerjordan1998@gmail.com>
2024-09-30 03:39:46 +0000

    val is not optional

--- train_gpt2.py
+++ train_gpt2.py
@@ -255,8 +255,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     # file system input / output
-    parser.add_argument("--input_bin", type=str, default="dev/data/tinyshakespeare/tiny_shakespeare_val.bin", help="input .bin to train on")
-    parser.add_argument("--input_val_bin", type=str, default="", help="input .bin to eval validation loss on")
+    parser.add_argument("--input_bin", type=str, help="input .bin to train on")
+    parser.add_argument("--input_val_bin", type=str, help="input .bin to eval validation loss on")
     parser.add_argument("--output_dir", type=str, default="", help="output directory to which to write logs and checkpoints")
     parser.add_argument("--model", type=str, default="d12", help="d12|d24|d36|d48")
     # token layout for each step of the optimization
@@ -298,9 +298,7 @@ if __name__ == "__main__":
 
     # load tokens
     train_loader = DistributedDataLoader(args.input_bin, B, T, ddp_rank, ddp_world_size)
-    val_loader = None
-    if args.input_val_bin:
-        val_loader = DistributedDataLoader(args.input_val_bin, B, T, ddp_rank, ddp_world_size)
+    val_loader = DistributedDataLoader(args.input_val_bin, B, T, ddp_rank, ddp_world_size)
     x, y = train_loader.next_batch()
 
     # init the model from scratch
