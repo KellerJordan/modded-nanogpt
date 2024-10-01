0a121b8f38408715b67e78e278e0faec3593738b
KellerJordan <kellerjordan1998@gmail.com>
2024-10-01 04:00:12 +0000

    .

--- train_gpt2.py
+++ train_gpt2.py
@@ -172,6 +172,7 @@ class GPT(nn.Module):
         if targets is not None:
             # if we are given some desired targets also calculate the loss
             logits = self.lm_head(x)
+            logits = logits.float() # for some reason, when using bfloat16 ctx and torch.compile, adding this speeds things up by 8%.
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
         else:
             # inference-time mini-optimization: only forward the lm_head on the very last position
