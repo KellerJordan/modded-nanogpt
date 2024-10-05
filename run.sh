b7b8313148b86c20457b52594570738049bd55f5
KellerJordan <kellerjordan1998@gmail.com>
2024-10-05 17:19:27 +0000

    .

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ torchrun --standalone --nproc_per_node=8 train_gpt2.py \
     --sequence_length 1024 \
     --val_loss_every 128 \
     --num_iterations 7000 \
-    --weight_decay 0.1 \
+    --weight_decay 0.0 \
     --learning_rate 0.0018 \
-    --warmup_iters 250 \
+    --warmup_iters 0 \
     --warmdown_iters 2000
