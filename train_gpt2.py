4ddc110d9d947e03e05583d39643d45805015256
KellerJordan <kellerjordan1998@gmail.com>
2024-10-12 16:30:00 +0000

    .

--- train_gpt2.py
+++ train_gpt2.py
@@ -178,7 +178,7 @@ class MLP(nn.Module):
 
     def forward(self, x):
         x = self.c_fc(x)
-        x = F.relu(x).square() # https://arxiv.org/abs/2109.08668v2; suggested to me by @Grad62304977
+        x = F.relu(x).square() # https://arxiv.org/abs/2109.08668v2; ~1-2% better than GELU; suggested to me by @Grad62304977
         x = self.c_proj(x)
         return x
 
