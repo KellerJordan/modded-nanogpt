c5f8ff9ec066b1d1a88849c2fb61303d550b79cb
Keller Jordan <kellerjordan1998@gmail.com>
2024-10-24 18:40:14 -0700

    Update cached_fineweb10B.py

--- data/cached_fineweb10B.py
+++ data/cached_fineweb10B.py
@@ -9,7 +9,7 @@ def get(fname):
         hf_hub_download(repo_id="kjj0/fineweb10B-gpt2", filename=fname,
                         repo_type="dataset", local_dir=local_dir)
 get("fineweb_val_%06d.bin" % 0)
-num_chunks = 104 # full fineweb10B. Each chunk is ~98.5M tokens
+num_chunks = 103 # full fineweb10B. Each chunk is ~98.5M tokens
 if len(sys.argv) >= 2: # we can pass an argument to download less
     num_chunks = int(sys.argv[1])
 for i in range(1, num_chunks+1):
