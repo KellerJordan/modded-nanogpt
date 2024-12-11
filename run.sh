f68cf6472429558e32b9cbb2c015bdb53b8a2369
Egil Martinsson <egil.martinsson@gmail.com>
2024-12-11 15:45:43 -0800

    A100 Adaptations

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-torchrun --standalone --nproc_per_node=8 train_gpt2.py
+# torchrun --standalone --nproc_per_node=8 train_gpt2.py
+torchrun --standalone --nproc_per_node=1 train_gpt2.py --train.batch_size 8 --train.sequence_length 32768
