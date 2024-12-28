5c7ba6ac06a6a4df09c1e603af18c9f145fd3421
Logan Hallee <lhallee@udel.edu>
2024-12-27 22:50:32 -0500

    Update benchmark_esm.py

--- benchmark_esm.py
+++ benchmark_esm.py
@@ -27,12 +27,12 @@ def main():
         # Load the ESM tokenizer and model
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         model_names = {
-            #'facebook/esm2_t6_8M_UR50D': 'ESM2-8M',
-            #'facebook/esm2_t12_35M_UR50D': 'ESM2-35M',
+            'facebook/esm2_t6_8M_UR50D': 'ESM2-8M',
+            'facebook/esm2_t12_35M_UR50D': 'ESM2-35M',
             'Synthyra/ESMplusplus_small': 'ESMC-300M',
-            #'facebook/esm2_t30_150M_UR50D': 'ESM2-150M',
+            'facebook/esm2_t30_150M_UR50D': 'ESM2-150M',
             'Synthyra/ESMplusplus_large': 'ESMC-600M',
-            #'facebook/esm2_t33_650M_UR50D': 'ESM2-650M'
+            'facebook/esm2_t33_650M_UR50D': 'ESM2-650M'
         }
 
         mask_rate, batch_size = 0.15, 4
