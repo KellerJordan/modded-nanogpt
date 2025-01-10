f695bcbfb4fd55f8144a17bcc8fd00a1e1cab81f
Alex2034 <fokin.aa@phystech.edu>
2025-01-11 02:05:26 +0300

    learnable curvature + fineweb

--- srun.sh
+++ srun.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
-# Sequence of curvature values
-curvatures=(100.0 200.0 500.0 1000.0)
+curvatures=(10. 30. 100. 300. 1000.)
+seeds=(1 2 3)
 
-# Sequence of seeds
-seeds=(1 2 3 4)
-
-# First, run Euclidean training with all seeds
 for seed in "${seeds[@]}"; do
-    echo "Running Euclidean model training with seed=${seed}"
-    CUDA_VISIBLE_DEVICES=4,5 torchrun --standalone --nproc_per_node=2 train_gpt2_hyp.py --lm_head 'euc' --seed "${seed}" > logs/logs_euclidean_seed_${seed}.txt 2>&1
-    echo "Finished Euclidean run with seed=${seed}, logs saved to logs/logs_euclidean_seed_${seed}.txt"
+    echo "Running script with euclidean head and seed=${seed}"
+    CUDA_VISIBLE_DEVICES=1,2 torchrun --standalone --nproc_per_node=2 \
+        train_gpt2_hyp.py \
+        --lm_head 'euc' \
+        --seed "${seed}" \
+        > logs/fineweb_euc_seed_${seed}.txt 2>&1
+    echo "Finished run with euclidean head and seed=${seed}, logs saved to logs/fineweb_euc_seed_${seed}.txt"
 done
 
-# Now, run training for each curvature and each seed
 for curvature in "${curvatures[@]}"; do
     for seed in "${seeds[@]}"; do
-        echo "Running script with curvature=${curvature} and seed=${seed}"
-        CUDA_VISIBLE_DEVICES=4,5 torchrun --standalone --nproc_per_node=2 train_gpt2_hyp.py --lm_head 'hyp' --curvature "${curvature}" --seed "${seed}" > logs/logs_curvature_${curvature}_seed_${seed}.txt 2>&1
-        echo "Finished run with curvature=${curvature} and seed=${seed}, logs saved to logs/logs_curvature_${curvature}_seed_${seed}.txt"
+        echo "Running script with fixed curvature=${curvature} and seed=${seed}"
+        CUDA_VISIBLE_DEVICES=1,2 torchrun --standalone --nproc_per_node=2 \
+            train_gpt2_hyp.py \
+            --lm_head 'hyp' \
+            --curvature "${curvature}" \
+            --seed "${seed}" \
+            > logs/fineweb_curvature_${curvature}_seed_${seed}.txt 2>&1
+        echo "Finished run with curvature=${curvature} and seed=${seed}, logs saved to logs/fineweb_curvature_${curvature}_seed_${seed}.txt"
+    done
+    # and now learnable
+    for seed in "${seeds[@]}"; do
+        echo "Running script with learnable curvature from ${curvature} and seed=${seed}"
+        CUDA_VISIBLE_DEVICES=1,2 torchrun --standalone --nproc_per_node=2 \
+            train_gpt2_hyp.py \
+            --lm_head 'hyp' \
+            --learnable True \
+            --curvature "${curvature}" \
+            --seed "${seed}" \
+            > logs/fineweb_learnable_curvature_${curvature}_seed_${seed}.txt 2>&1
+        echo "Finished run with learnable curvature=${curvature} and seed=${seed}, logs saved to logs/fineweb_learnable_curvature_${curvature}_seed_${seed}.txt"
     done
 done
 
