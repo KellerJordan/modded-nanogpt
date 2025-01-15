c3b7b407695b88fa773db51bb2cdecca2dee4fbf
You Jiacheng <1503679330@qq.com>
2025-01-15 13:25:22 +0800

    .

--- train_gpt.py
+++ train_gpt.py
@@ -324,7 +324,7 @@ class ValueEmbedding(nn.Module):
 
     def forward(self, input_seq) -> "list[Tensor | None]":
         ve = [emb(input_seq) for emb in self.embed]
-        # 012 ... 012 structure on token value embeddings by @YouJiacheng, improved upon @leloykun's U-net structure
+        # 012 ... 012 structure on token value embeddings by @YouJiacheng, improved on @leloykun's U-net structure
         ve = [ve[0], ve[1], ve[2], None, None, None, None, None, None, ve[0], ve[1], ve[2]]
         return ve
 
