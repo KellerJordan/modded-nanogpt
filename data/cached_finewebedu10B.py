27352f779f34cc806a3625d362cc79049c629fb0
Keller Jordan <kellerjordan1998@gmail.com>
2025-01-19 06:28:56 +0000

    .

--- data/cached_finewebedu10B.py
+++ data/cached_finewebedu10B.py
@@ -9,7 +9,7 @@ def get(fname):
         hf_hub_download(repo_id="kjj0/finewebedu10B-gpt2", filename=fname,
                         repo_type="dataset", local_dir=local_dir)
 get("finewebedu_val_%06d.bin" % 0)
-num_chunks = 103 # full FinewebEDU10B. Each chunk is 100M tokens
+num_chunks = 99 # full FinewebEDU10B. Each chunk is 100M tokens
 if len(sys.argv) >= 2: # we can pass an argument to download less
     num_chunks = int(sys.argv[1])
 for i in range(1, num_chunks+1):
