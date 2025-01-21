3f96031ed6578bcbd5b25c5ed586d73dc02ee189
Franz Louis Cesista <franzlouiscesista@gmail.com>
2025-01-22 07:46:52 +0800

    increase num iterations

--- train_gpt.py
+++ train_gpt.py
@@ -460,7 +460,7 @@ class Hyperparameters:
     val_tokens = 10485760 # how many tokens of validation data? it's important to keep this fixed for consistent comparisons
     # optimization
     batch_size = 8*64*1024 # batch size in tokens
-    num_iterations = 1393 # number of iterations to run
+    num_iterations = 1395 # number of iterations to run
     cooldown_frac = 0.4 # fraction of training spent cooling down the learning rate
     # evaluation and logging
     val_loss_every = 125 # every how many steps to evaluate val loss? 0 for only at the end
