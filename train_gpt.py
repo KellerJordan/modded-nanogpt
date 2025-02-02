e72a958483626bf4fe65fc69871ba4f2b3a938c6
KellerJordan <kellerjordan1998@gmail.com>
2025-02-02 05:49:25 +0000

    .

--- train_gpt.py
+++ train_gpt.py
@@ -536,7 +536,7 @@ for opt in optimizers:
 # learning rate schedule: stable then decay
 def get_lr(step: int):
     x = step / args.num_iterations # progress in training
-    assert 0 <= x <= 1
+    assert 0 <= x < 1
     if x < 1 - args.cooldown_frac:
         return 1.0
     else:
@@ -548,9 +548,11 @@ def get_lr(step: int):
 def get_window_size_blocks_helper(window_size: int):
     return torch.tensor(window_size // 128, dtype=torch.int32, pin_memory=True).cuda(non_blocking=True)
 def get_window_size_blocks(step: int):
+    x = step / args.num_iterations # progress in training
+    assert 0 <= x <= 1
     # Linearly increase the block-wise sliding window size over training 128 -> 1792
     # increase by @fernbear.bsky.social; block-wise by @YouJiacheng
-    window_size = next_multiple_of_n(1728 * step / train_steps, n=128)
+    window_size = next_multiple_of_n(1728 * x, n=128)
     return get_window_size_blocks_helper(window_size)
 
 model: nn.Module = torch.compile(model, dynamic=False)
