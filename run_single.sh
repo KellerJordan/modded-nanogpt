57b89aeb78e5e50354ea9fdb7b692a48470f71e7
Alex2034 <fokin.aa@phystech.edu>
2025-04-12 18:44:31 +0300

    character tokenizer

--- run_single.sh
+++ run_single.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=0,1 torchrun --standalone --nproc_per_node=2 \
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=1,3 torchrun --standalone --nproc_per_node=2 \
     train_gpt2_main.py \
-    --data_path "data/fineweb10B" \
+    --data_path "data/shakespeare_char" \
     --device_batch_size 16 \
     --batch_size 32 \
     --num_iterations 300 \
-    --generate_every 300 \
+    --generate_every 100 \
     --n_heads 6 \
     --n_layers 6 \
     --head_dim 16 \
