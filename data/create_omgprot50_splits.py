7346da064865604b4c4fb96563de26da2e60998a
Logan Hallee <lhallee@udel.edu>
2025-07-03 00:00:40 -0400

    update

--- data/create_omgprot50_splits.py
+++ data/create_omgprot50_splits.py
@@ -1,5 +1,14 @@
+import argparse
 from datasets import load_dataset, DatasetDict
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--token', type=str, defualt=None)
+
+args = parser.parse_args()
+
+if args.token:
+    import huggingface_hub
+    huggingface_hub.login(token=args.token)
 
 data = load_dataset('tattabio/OMG_prot50', split='train').remove_columns('id').shuffle(seed=11)
 #data = data.cast_column('sequence', Value(dtype='string'))
--- data/create_uniref50_splits.py
+++ data/create_uniref50_splits.py
@@ -1,11 +1,20 @@
+import argparse
 from datasets import load_dataset, DatasetDict, concatenate_datasets
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--token', type=str, defualt=None)
+
+args = parser.parse_args()
+
+if args.token:
+    import huggingface_hub
+    huggingface_hub.login(token=args.token)
 
 data = load_dataset('agemagician/uniref50_09012025').remove_columns('id').remove_columns('name').shuffle(seed=11)
 data = data.rename_column('text', 'sequence')
 print(data)
 
-data = concatenate_datasets([data['train'], data['valid'], data['test']])
+data = concatenate_datasets([data['train'], data['validation'], data['test']])
 
 data = data.train_test_split(test_size=20000, seed=22)
 
