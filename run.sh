a4f13ce840e6dc3a74b97d4679530b417d7f203d
Chris McCormick <chrisjmccormick@gmail.com>
2025-12-11 00:00:57 -0800

    Updating to latest record

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-torchrun --standalone --nproc_per_node=8 train_gpt.py
+torchrun --standalone --nproc_per_node=8 train_gpt_12-10-baseline.py
+torchrun --standalone --nproc_per_node=8 train_gpt_gh200_12-10.py
+torchrun --standalone --nproc_per_node=8 train_gpt_12-10-baseline.py
+torchrun --standalone --nproc_per_node=8 train_gpt_gh200_12-10.py
+torchrun --standalone --nproc_per_node=8 train_gpt_12-10-baseline.py
+torchrun --standalone --nproc_per_node=8 train_gpt_gh200_12-10.py
