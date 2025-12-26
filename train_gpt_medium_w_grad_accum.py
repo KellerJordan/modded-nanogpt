ed371c2251c26633c937a3b28bd1dbbc5ab781e1
HaileyStorm <HaileyCollet@gmail.com>
2025-12-26 13:43:54 -0700

    2xH100 run

--- train_gpt_medium_w_grad_accum.py
+++ train_gpt_medium_w_grad_accum.py
@@ -18,7 +18,7 @@ import torch.distributed as dist
 # use of FlexAttention contributed by @KoszarskyB
 from torch.nn.attention.flex_attention import BlockMask, flex_attention
 torch._inductor.config.coordinate_descent_tuning = True # we allow this flag for medium track
-torch._dynamo.config.compiled_autograd = True
+torch._dynamo.config.compiled_autograd = False
 
 # -----------------------------------------------------------------------------
 # Muon optimizer
