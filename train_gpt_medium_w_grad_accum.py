d507358da43b8523486fc1ca43095843c017e080
HaileyStorm <HaileyCollet@gmail.com>
2025-12-26 13:51:36 -0700

    2xH100 run

--- train_gpt_medium_w_grad_accum.py
+++ train_gpt_medium_w_grad_accum.py
@@ -17,7 +17,7 @@ import torch.nn.functional as F
 import torch.distributed as dist
 # use of FlexAttention contributed by @KoszarskyB
 from torch.nn.attention.flex_attention import BlockMask, flex_attention
-torch._inductor.config.coordinate_descent_tuning = True # we allow this flag for medium track
+torch._inductor.config.coordinate_descent_tuning = True # we allow this flag for medium track; false for rocm / single-GPU
 torch._dynamo.config.compiled_autograd = False
 
 # -----------------------------------------------------------------------------
