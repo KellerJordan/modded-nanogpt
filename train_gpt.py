5cf20284ae37ebdc48910cc1cd1ce66d0c969cb0
Roee Shenberg <shenberg@gmail.com>
2026-02-02 11:04:50 +0100

    debugging

--- train_gpt.py
+++ train_gpt.py
@@ -220,6 +220,7 @@ def a2a_prefwd_start_1(idxes_np, N, rank, world):
     send_counts[rank] = 0
     # remove our own indexes from the send list
     send_idxes = torch.cat([send_idxes[:insertion_points[rank]], send_idxes[insertion_points[rank + 1:]]])
+    print(f"{rank = }, {send_counts = }, {send_counts.sum() = }, {send_idxes.shape = }")
 
     # share the send counts so that each rank will know how many rows
     # to expect from every other rank
