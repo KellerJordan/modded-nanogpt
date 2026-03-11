aec0977b345ff65f551cb5d8737a3cefc6edd391
Keller Jordan <kellerjordan1998@gmail.com>
2026-03-10 22:15:05 -0700

    .

--- records/track_2_medium/OptimizationLeaderboard/simple_train_gpt.py
+++ records/track_2_medium/OptimizationLeaderboard/simple_train_gpt.py
@@ -1,8 +1,11 @@
 """
 simple_train_gpt.py
 
-This code descends from the [NanoGPT speedrun](https://github.com/KellerJordan/modded-nanogpt).
-See the repo for a full list of contributors.
+This file descends from the [NanoGPT speedrun](https://github.com/KellerJordan/modded-nanogpt).
+It was prepared as a simplified version of the speedrun which removes non-standard parameters
+(hence we remove value embeddings and skip connections, and keep ReLU^2 and softcap).
+
+See the speedrun repo for a full list of contributors.
 """
 
 import os
