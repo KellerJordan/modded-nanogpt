8789a17777b39153a8644495596c7ae13ed48818
Keller Jordan <kellerjordan1998@gmail.com>
2026-04-25 16:06:47 -0700

    Update train_gpt_simple.py

--- records/track_3_optimization/train_gpt_simple.py
+++ records/track_3_optimization/train_gpt_simple.py
@@ -3,8 +3,6 @@ train_gpt_simple.py
 
 This file descends from the [NanoGPT speedrun](https://github.com/KellerJordan/modded-nanogpt).
 It was prepared as a simplified version of the speedrun for use in neural net optimization research.
-In particular, we remove non-standard parameters (e.g., value embeddings) in order to simplfy experiments.
-See the speedrun repo for a full list of contributors.
 """
 
 import os
