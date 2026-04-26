efabd7ebf365046d69b82150571312c12a7126c3
Keller Jordan <kellerjordan1998@gmail.com>
2026-04-26 11:37:04 -0700

    Update train_gpt_simple.py

--- records/track_3_optimization/train_gpt_simple.py
+++ records/track_3_optimization/train_gpt_simple.py
@@ -191,14 +191,9 @@ def zeropower_via_newtonschulz5(G: Tensor) -> Tensor:
 
     # Ensure spectral norm is at most 1
     X = X / (X.norm(dim=(-2, -1), keepdim=True) + 1e-7)
-    # Perform the NS iterations
-    for a, b, c in [
-        (4.0848, -6.8946, 2.9270),
-        (3.9505, -6.3029, 2.6377),
-        (3.7418, -5.5913, 2.3037),
-        (2.8769, -3.1427, 1.2046),
-        (2.8366, -3.0525, 1.2012),
-    ]:
+    # Perform the NS iterations, not optimizing for speed
+    a, b, c = 2, -1.5, 0.5
+    for _ in range(12):
         A = X @ X.mT
         B = b * A + c * A @ A
         X = a * X + B @ X
