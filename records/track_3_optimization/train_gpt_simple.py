779ac8a677b1e91278bcaaf27fafde89dfb92a6f
Keller Jordan <kellerjordan1998@gmail.com>
2026-04-26 14:29:44 -0700

    .

--- records/track_3_optimization/train_gpt_simple.py
+++ records/track_3_optimization/train_gpt_simple.py
@@ -264,7 +264,7 @@ model.compile(dynamic=False)
 ########################################
 
 # we want to minimize this while still reaching 3.28 val loss
-train_steps = 3500
+train_steps = 3550
 
 # initialize model parameters
 for name, p in model.named_parameters():
