a5843253e798b0f59d3fb25714392b3a435e0626
Keller Jordan <kellerjordan1998@gmail.com>
2026-05-03 14:48:47 -0700

    .

--- records/track_3_optimization/make_figures.py
+++ records/track_3_optimization/make_figures.py
@@ -20,7 +20,7 @@ logfiles = {
     8: 'NorMuonH',
     9: 'NorMuon w/ update clamping',
     10: 'NorMuon',
-    11: 'Contra-NorMuon w/ update clamping',
+    11: 'ContraNorMuon w/ update clamping',
 }
 readme_rows = {}
 row_pattern = re.compile(
