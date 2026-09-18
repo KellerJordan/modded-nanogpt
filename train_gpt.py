c476943203f59301249b3bb0de745225c084e228
Larry Dial <42926649+ClassicLarry@users.noreply.github.com>
2026-09-17 21:25:25 -0700

    Update train_gpt.py

--- train_gpt.py
+++ train_gpt.py
@@ -2071,7 +2071,7 @@ class Hyperparameters:
     # batch sizes
     val_batch_size: int = 4 * 64 * 1024 * 8
     # schedule
-    num_scheduled_iterations: int = 1270  # number of steps to complete lr and ws schedule
+    num_scheduled_iterations: int = 1250  # number of steps to complete lr and ws schedule
     num_extension_iterations: int = int(os.environ.get("NUM_EXTENSION_ITERATIONS", "45"))  # number of steps to continue training at final lr and ws
     # evaluation and logging
     run_id: str = f"{uuid.uuid4()}"
